09d47934b75d3f6a7b1a68b4064e74ff47aa842c
Israel Oluwasegun <marksegman@outlook.com>
2025-07-03 22:48:13 +0100

    add agg and window func

---
+++ database-adv-script/aggregations_and_window_function.sql
@@ -0,0 +1,14 @@
+USE alx_airbnb;
+SELECT  user.user_id,  COUNT(booking.booking_id)
+FROM booking
+INNER JOIN user
+ON booking.user_id = user.user_id
+GROUP BY user.user_id;
+
+SELECT 
+property_id,
+COUNT(*) AS total_bookings,
+RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM booking
+GROUP BY property_id;
+
